793014182f2a7d38ad2ec8f94b03c6fabf009265
John Ali <alionuche2008@gmail.com>
2025-07-25 10:31:04 +0100

    kk

--- Models/Block.cs
+++ Models/Block.cs
@@ -1,20 +1,19 @@
-﻿namespace OluBackendApp.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OluBackendApp.Models
 {
     public class Block
     {
         public int Id { get; set; }
+
         public string BlockerId { get; set; } = default!;
         public string BlockedId { get; set; } = default!;
         public DateTime CreatedAt { get; set; }
 
-     
-
+        [ForeignKey(nameof(BlockerId))]
         public ApplicationUser Blocker { get; set; } = null!;
 
+        [ForeignKey(nameof(BlockedId))]
         public ApplicationUser Blocked { get; set; } = null!;
-
-       
     }
 }
-
-
